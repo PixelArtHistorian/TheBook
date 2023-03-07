92e6a1f2cab564c7b10b498e020919b5c8624cbe
Alberto Diana <a.diana.dev@gmail.com>
2023-03-07 21:53:50 +0100

    fix formatting

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,7 +1,8 @@
 use rand::Rng;
 
 fn main() {
-    let x: u32 = rand::thread_rng().gen_range(1..=10);
+    let x = rand::thread_rng().gen_range(1..=10);
+    
     if x % 2 == 0{
         println!("{x} is even")
     }else{

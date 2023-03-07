945caefc377f98460798de470215c5519d5a5a23
A.Diana <a.diana.dev@gmail.com>
2023-03-07 21:38:32 +0100

    add function with return value

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
     another_function(3);
-
-    print_labeled_measurement(5, 'w');
+    let w = 5;
+    print_labeled_measurement(w, 'w');
+    let h = calculate_height(w);
+    print_labeled_measurement(h, 'h');
 }
 
 fn another_function(x: i32){
@@ -9,5 +11,8 @@ fn another_function(x: i32){
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char){
-    println!("The measure is : {value} {unit_label}")
+    println!("The measure is : {value} {unit_label}");
+}
+fn calculate_height(w: i32) -> i32{
+    w * 2
 }

a062d3698c9f0c1c54ca18fa4a24d5ddb613c80c
Alberto Diana <a.diana.dev@gmail.com>
2023-03-08 21:51:16 +0100

    add another fn

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -3,9 +3,13 @@ use rand::Rng;
 fn main() {
     let x = rand::thread_rng().gen_range(1..=10);
     
-    if x % 2 == 0{
+    if is_even(x){
         println!("{x} is even")
     }else{
         println!("{x} is not even")
     }
+}
+
+fn is_even(x: i32) -> bool{
+    x % 2 == 0
 }

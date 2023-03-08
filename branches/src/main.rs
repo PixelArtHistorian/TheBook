12b0e3e5aadd96b1ccbeaea116e8a1c97299efa3
Alberto Diana <a.diana.dev@gmail.com>
2023-03-08 22:38:07 +0100

    add abs function

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,15 +1,22 @@
 use rand::Rng;
 
 fn main() {
-    let x = rand::thread_rng().gen_range(1..=10);
-    
-    if is_even(x){
-        println!("{x} is even")
-    }else{
-        println!("{x} is not even")
+    let x = rand::thread_rng().gen_range(-10..=10);
+
+    if is_even(x) {
+        println!("{x} is even");
+    } else {
+        println!("{x} is not even");
     }
+
+    let y = abs(x);
+    println!("The abs value of x is {y}");
 }
 
-fn is_even(x: i32) -> bool{
+fn is_even(x: i32) -> bool {
     x % 2 == 0
+}
+
+fn abs(x: i32) -> i32 {
+    if x >= 0 { x } else { -x }
 }
